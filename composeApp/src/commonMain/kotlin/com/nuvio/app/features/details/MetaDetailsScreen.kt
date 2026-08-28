5b54fa92416ecf884a053f35c6eb432980bf6725
siriusvoid <271671657+siriusvoid@users.noreply.github.com>
2026-08-28 09:10:55 +0500

    fix: stop details page overscrolling past its content

--- composeApp/src/commonMain/kotlin/com/nuvio/app/features/details/MetaDetailsScreen.kt
+++ composeApp/src/commonMain/kotlin/com/nuvio/app/features/details/MetaDetailsScreen.kt
@@ -44,6 +44,8 @@ import com.nuvio.app.core.ui.NuvioLoadingIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.foundation.LocalOverscrollFactory
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.derivedStateOf
@@ -1038,6 +1040,7 @@ fun MetaDetailsScreen(
                                     )
                                 }
                             }
+                            CompositionLocalProvider(LocalOverscrollFactory provides null) {
                             LazyColumn(
                                 state = listState,
                                 modifier = Modifier
@@ -1176,6 +1179,7 @@ fun MetaDetailsScreen(
                                 item(key = "detail-bottom-spacer") {
                                     Spacer(modifier = Modifier.height(nuvioSafeBottomPadding(32.dp)))
                                 }
+                            }
                             }
 
                             if (backgroundMode.usesBackdropBackground && deferredMetaWorkAllowed && heroHeightPx.intValue > 0) {
