16bab8a8613ef34b8bbdb3d22b0688d854db53ed
siriusvoid <271671657+siriusvoid@users.noreply.github.com>
2026-09-18 22:25:02 +0500

    perf(image): keep the iOS image disk cache in the caches directory

--- composeApp/src/iosMain/kotlin/com/nuvio/app/core/ui/PlatformImageLoader.ios.kt
+++ composeApp/src/iosMain/kotlin/com/nuvio/app/core/ui/PlatformImageLoader.ios.kt
@@ -1,5 +1,22 @@
 package com.nuvio.app.core.ui
 
 import coil3.ImageLoader
+import coil3.disk.DiskCache
+import okio.FileSystem
+import okio.Path.Companion.toPath
+import platform.Foundation.NSCachesDirectory
+import platform.Foundation.NSSearchPathForDirectoriesInDomains
+import platform.Foundation.NSUserDomainMask
 
-internal actual fun ImageLoader.Builder.configurePlatformImageLoader(): ImageLoader.Builder = this
+// Coil's default iOS disk cache lives in the temp directory, which iOS clears whenever the app isn't running.
+internal actual fun ImageLoader.Builder.configurePlatformImageLoader(): ImageLoader.Builder {
+    val cachesDirectory = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, true)
+        .firstOrNull() as? String
+        ?: return this
+    return diskCache {
+        DiskCache.Builder()
+            .fileSystem(FileSystem.SYSTEM)
+            .directory("$cachesDirectory/image_cache".toPath())
+            .build()
+    }
+}
